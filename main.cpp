7b3de2b0645af59e0ebc137af519317154c480c7
Madmaxvas <63355084+Madmaxvas@users.noreply.github.com>
2026-03-18 22:08:36 +0300

    DONE_END

--- main.cpp
+++ main.cpp
@@ -24,47 +24,48 @@ Vec< List< T >* > balanced_move(Vec< List< T >* > v, size_t k) {
         }
     }
 
-    List<T>** all_nodes = nullptr;
-    List<T>** new_data = nullptr;
+    List<T>** new_data = new List<T>*[k](); 
 
     try {
-        if (total_nodes > 0) {
-            all_nodes = new List<T>*[total_nodes];
+        if (total_nodes == 0) return { new_data, k, k };
+
+        List<T>** all_nodes = new List<T>*[total_nodes];
+        try {
+            size_t idx = 0;
+            for (size_t i = 0; i < v.s; ++i) {
+                for (List<T>* curr = v.data[i]; curr; ) {
+                    all_nodes[idx++] = curr;
+                    curr = curr->next;
+                }
+            }
+
+            size_t nodes_per_list = total_nodes / k;
+            size_t extra = total_nodes % k;
+            size_t current_node_idx = 0;
+
+            for (size_t i = 0; i < k; ++i) {
+                size_t count = nodes_per_list + (i < extra ? 1 : 0);
+                if (count > 0) {
+                    new_data[i] = all_nodes[current_node_idx];
+                    for (size_t j = 0; j < count - 1; ++j) {
+                        all_nodes[current_node_idx + j]->next = all_nodes[current_node_idx + j + 1];
+                    }
+                    all_nodes[current_node_idx + count - 1]->next = nullptr;
+                    current_node_idx += count;
+                } else {
+                    new_data[i] = nullptr;
+                }
+            }
+            delete[] all_nodes;
+        } catch (...) {
+            delete[] all_nodes;
+            throw;
         }
-        new_data = new List<T>*[k]();
     } catch (...) {
-        delete[] all_nodes;
         delete[] new_data;
         throw;
     }
 
-    size_t idx = 0;
-    for (size_t i = 0; i < v.s; ++i) {
-        for (List<T>* curr = v.data[i]; curr; curr = curr->next) {
-            all_nodes[idx++] = curr;
-        }
-    }
-
-    size_t nodes_per_list = total_nodes / k;
-    size_t extra = total_nodes % k;
-    size_t current_node_idx = 0;
-
-    for (size_t i = 0; i < k; ++i) {
-        size_t count = nodes_per_list + (i < extra ? 1 : 0);
-        if (count > 0) {
-            new_data[i] = all_nodes[current_node_idx];
-            for (size_t j = 0; j < count - 1; ++j) {
-                all_nodes[current_node_idx + j]->next = all_nodes[current_node_idx + j + 1];
-            }
-            all_nodes[current_node_idx + count - 1]->next = nullptr;
-            current_node_idx += count;
-        } else {
-            new_data[i] = nullptr;
-        }
-    }
-
-    delete[] all_nodes;
-    
     return { new_data, k, k };
 }
 
